f06b2cc47af1a8a37f1063dc05a8b4003f308b61
postre554 <moglimon554@gmail.com>
2023-05-28 22:48:47 -0400

    menu 3 set

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,23 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var pageNext = document.querySelector(".page.next");
+    var pagePrev = document.querySelector(".page.prev");
+
+ 
+    function cambiarTextoEnlace(elemento, nuevoTexto) {
+      if (elemento) {
+        var link = elemento.querySelector("a");
+        if (link) {
+          link.textContent = nuevoTexto;
+        }
+      }
+    }
+
+    cambiarTextoEnlace(pageNext, "Siguiente");
+
+    cambiarTextoEnlace(pagePrev, "Anterior");
+  });
